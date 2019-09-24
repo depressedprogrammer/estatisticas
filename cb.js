e52d64c1a6f1246b703cb0680006ccaf3760edc9
depressedprogrammer <ryuuji.aisaka7@gmail.com>
2019-09-24 17:29:57 -0300

    exibir vetor

---
+++ cb.js
@@ -0,0 +1,28 @@
+var rol = document.getElementById('entrada');
+var btok = document.getElementById('btok');
+var tabela = document.createElement('table');
+var entradas = []
+
+btok.onclick = function(){
+    var texto = rol.value.split(" ");
+    
+    entradas = texto.map(function (x) { 
+        return parseInt(x, 10); 
+    });
+    
+    var saida = document.getElementById("rol");
+    saida.innerHTML = "";
+
+    var desordenado = document.createTextNode(entradas);
+    var p1 = document.createElement('p');
+    p1.appendChild(desordenado);
+    saida.appendChild(p1);
+
+    var ordenado = document.createTextNode(entradas.sort());
+    var p2 = document.createElement('p');
+    p2.appendChild(ordenado);
+    saida.appendChild(p2);
+}
+
+var total = document.getElementById('total');
+total.appendChild(document.createTextNode())
--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var logo = document.getElementById('logo');
-var divbotoes = document.getElementById('botoes');
-
-conteudo = ['calculo basico']
-for (x of conteudo){
-  divbotoes.innerHtml = "";
-  let txtnode = document.createTextNode(x);
-  let botao = document.createElement('button');
-  
-  botao.appendChild(txtnode);
-  botao.setAttribute('style','background-color: light gray;border:none;padding:10px 50px;');
-  divbotoes.setAttribute('align','center');
-  divbotoes.appendChild(botao);
-}
-
+logo.setAttribute('src','https://66.media.tumblr.com/4211d1c5cf721bf5da87229b492f6526/tumblr_plrx8mfmOA1sguk2k_540.gif')
 logo.setAttribute('style','width:auto;height:200px;');
+
+var conteudo = ['calculo basico','botao aleatorio', 'uh'];
+var links = ['cb.html','https://theuselessweb.com/','#']
+
+function updateBotoes(){
+  const divbotoes = document.getElementById('botoes');
+  divbotoes.innerHtml = "";
+  divbotoes.setAttribute('align','center');
+  for (x of conteudo){
+    
+    let txtnode = document.createTextNode(x);
+    let botao = document.createElement('button');
+    botao.setAttribute('onclick','window.location.href = "' + links[conteudo.indexOf(x)] + '"');
+    botao.appendChild(txtnode);
+    botao.setAttribute('style','cursor:pointer;background-color: gray;border:none;padding:10px 50px; margin-top:10px; margin-bottom:10px;');
+    divbotoes.appendChild(botao);
+    divbotoes.appendChild(document.createElement('br'));
+  }
+}
+updateBotoes();
