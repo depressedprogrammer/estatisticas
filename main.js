4f62d097c72e55b18ad586f9b2922d8c9d85e991
depressedprogrammer <55628124+depressedprogrammer@users.noreply.github.com>
2019-09-24 10:35:12 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+var logo = document.getElementById('logo');
+var divbotoes = document.getElementById('botoes');
+
+conteudo = ['calculo basico']
+for (x of conteudo){
+  divbotoes.innerHtml = "";
+  let txtnode = createTextNode(x);
+  let botao = createElement('button');
+  
+  botao.appendChild(txtnode);
+  divbotoes.appendChild(botao);
+}
+
+logo.setAttribute = ('width','auto');
+logo.setAttribute = ('height', 200');
