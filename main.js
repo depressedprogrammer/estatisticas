f662292cb61370a280a350dd685d8d078aae6908
depressedprogrammer <ryuuji.aisaka7@gmail.com>
2019-09-24 11:01:39 -0300

    super

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ var divbotoes = document.getElementById('botoes');
 conteudo = ['calculo basico']
 for (x of conteudo){
   divbotoes.innerHtml = "";
-  let txtnode = createTextNode(x);
-  let botao = createElement('button');
+  let txtnode = document.createTextNode(x);
+  let botao = document.createElement('button');
   
   botao.appendChild(txtnode);
+  botao.setAttribute('style','background-color: light gray;border:none;padding:10px 50px;');
+  divbotoes.setAttribute('align','center');
   divbotoes.appendChild(botao);
 }
 
-logo.setAttribute = ('width','auto');
-logo.setAttribute = ('height', '200');
+logo.setAttribute('style','width:auto;height:200px;');
