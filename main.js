107172697fd98dbb6bcb903656020013e0221c87
depressedprogrammer <ryuuji.aisaka7@gmail.com>
2019-09-26 14:48:43 -0300

    working links

--- main.js
+++ main.js
@@ -5,21 +5,8 @@ function tituloprincipal(local){
 
 
 var conteudo = ['calculo basico','botao aleatorio', 'uh'];
-var links = ['cb.html','https://theuselessweb.com/','#']
-
-function updateBotoes(){
-  const divbotoes = document.getElementById('botoes');
-  divbotoes.innerHtml = "";
-  divbotoes.setAttribute('align','center');
-  for (x of conteudo){
-    
-    let txtnode = document.createTextNode(x);
-    let botao = document.createElement('button');
-    botao.setAttribute('onclick','window.location.href = "' + links[conteudo.indexOf(x)] + '"');
-    botao.appendChild(txtnode);
-    botao.setAttribute('style','cursor:pointer;background-color: gray;border:none;padding:10px 50px; margin-top:10px; margin-bottom:10px;');
-    divbotoes.appendChild(botao);
-    divbotoes.appendChild(document.createElement('br'));
-  }
-}
-updateBotoes();
+var links = ["cb.html","https://theuselessweb.com/","#"];
+var botoes = document.querySelectorAll("div[class='options']");
+botoes[0].onclick = function() {window.location.href = links[0];}
+botoes[1].onclick = function() {window.location.href = links[1];}
+botoes[2].onclick = function() {window.location.href = links[2];}
