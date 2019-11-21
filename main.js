acf4016da10e871b508ddb1a47cbd3870aece368
depressedprogrammer <55628124+depressedprogrammer@users.noreply.github.com>
2019-11-21 17:36:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function tituloprincipal(local){
 
 
 var conteudo = ['calculo basico','botao aleatorio', 'uh'];
-var links = ["cb.html","https://theuselessweb.com/","#"];
+var links = ["cb.html","https://theuselessweb.com/","uh.html"];
 var botoes = document.querySelectorAll("div[class='options']");
 botoes[0].onclick = function() {window.location.href = links[0];}
 botoes[1].onclick = function() {window.location.href = links[1];}
